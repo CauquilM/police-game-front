48746cba1fbcb68c0ff8410111850aa4810ed6a2
MathCoder <mathieufc31@gmail.com>
2024-05-31 15:02:52 +0200

    add vehicle types

--- src/components/TrafficStopComponent.vue
+++ src/components/TrafficStopComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <b-card :header="licensePlate" header-tag="header" title="Title">
-            <b-card-text>Header and footers using props.</b-card-text>
+        <b-card :header="vehicle.licensePlate" header-tag="header" :title="vehicle.vehicleType">
+            <b-card-text>Car {{ vehicle.vehicleType }}</b-card-text>
             <b-button href="#" variant="primary">Go somewhere</b-button>
         </b-card>
     </div>
@@ -12,28 +12,38 @@ export default {
     name: "TrafficStopComponent",
     data() {
         return {
-            licensePlate: "",
             alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
-                "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+                "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],   
+            vehicleTypes: [
+                "Sedan", "SUV", "Coupe", "Convertible", "Minivan", "Pickup Truck",
+                "Crossover", "Sports Car", "Luxury Car", "Electric Car", "Hybrid Car", "Diesel Car", "Compact Car",
+                "Subcompact Car", "Roadster", "Off-Road Vehicle", "Microcar", "Muscle Car"
+            ],
+            vehicle: {
+                licensePlate: "",
+                vehicleType: "",
+                vehicleInventory: []
+            }
         }
     },
     mounted() {
-        this.licensePlateRandomizer();
+        this.vehicleInitializer();
     },
     props: {
         isRefresh: Boolean
     },
     methods: {
-        licensePlateRandomizer() {
+        vehicleInitializer() {
             for (let index = 0; index < 3; index++) {
                 let random = parseInt(Math.random() * 26);
-                this.licensePlate = this.licensePlate.concat(this.alphabet[random]);
+                this.vehicle.licensePlate = this.vehicle.licensePlate.concat(this.alphabet[random]);
             }
-            this.licensePlate = this.licensePlate.concat('-');
+            this.vehicle.licensePlate = this.vehicle.licensePlate.concat('-');
             for (let index = 0; index < 3; index++) {
                 let random = parseInt(Math.random() * 10);
-                this.licensePlate = this.licensePlate.concat(random);
+                this.vehicle.licensePlate = this.vehicle.licensePlate.concat(random);
             }
+            this.vehicle.vehicleType = this.vehicleTypes[parseInt(Math.random() * this.vehicleTypes.length)];
         }
     }
 }
